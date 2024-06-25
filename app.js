d8c33b8947a5305ca25948cf37c5ed5e062b728b
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-06-26 01:04:50 +0300

    add route handlers for users

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const updateTour = (req, res) => {
   res.status(200).json({
     status: "success",
     data: {
-      tour: "<update tour tour>",
+      tour: "<update tour>",
     },
   });
 };
@@ -51,6 +51,47 @@ const deleteTour = (req, res) => {
     status: "success",
     data: {
       tour: null,
+    },
+  });
+};
+
+const getAllUsers = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      users: "<list of users>",
+    },
+  });
+};
+const getSingleUser = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: "<specified user>",
+    },
+  });
+};
+const createUser = (req, res) => {
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: "<create new user>",
+    },
+  });
+};
+const updateUser = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: "<update user>",
+    },
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(204).json({
+    status: "success",
+    data: {
+      user: null,
     },
   });
 };
