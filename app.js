3bbcab6d715e46981aaf117f015ec697e37fd0ce
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-06-26 00:54:24 +0300

    add get all tours route

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ dotenv.config();
 // Creating an Express app
 const app = express();
 
+// Routes Handlers
+app.get("/api/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      tours: "<list of tours>",
+    },
+  });
+});
+
 // Setting the port and starting the server
 console.log(process.env.PORT);
 const PORT = process.env.PORT || 3000;
