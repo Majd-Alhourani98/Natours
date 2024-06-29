040d89fd026190778423c579165aa069fc8d70c4
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-06-29 13:50:16 +0300

    add pagaination functionality

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,7 +21,7 @@ const getAllTours = async (req, res) => {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
     } else {
-      query.sort('-createdAt');
+      // query.sort('-createdAt');
     }
 
     // Limiting Fields
@@ -31,6 +31,12 @@ const getAllTours = async (req, res) => {
     } else {
       query = query.select('-__v');
     }
+
+    // pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 9;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
 
     const tours = await query;
     res.status(200).json({
