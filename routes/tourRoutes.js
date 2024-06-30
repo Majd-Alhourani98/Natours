58e5045ea467c6041621a7273b37a3ad709c5485
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-06-30 13:03:13 +0300

    add top 5 chepest tours route alias route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllTours, getSingleTour, createTour, updateTour, deleteTour } = require('./../controllers/tourController');
+const {
+  getAllTours,
+  getSingleTour,
+  createTour,
+  updateTour,
+  deleteTour,
+  topFiveCheapTours,
+} = require('./../controllers/tourController');
 
+router.route('/top-5-cheap-tours').get(topFiveCheapTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getSingleTour).patch(updateTour).delete(deleteTour);
 
