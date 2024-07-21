b4e834bdacb7a2f147713de1731f19c606789520
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-07-21 12:19:44 +0300

    implement a global error handling middleware

--- app.js
+++ app.js
@@ -21,10 +21,23 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/tours', tourRouter);
 app.use('/api/users', userRouter);
 
+// handle unhandled routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// Global Error Handling Middlware
+app.use((err, req, res, next) => {
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
