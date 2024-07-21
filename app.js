3fc0a195299fa6d470dd11ee009af3aec843e802
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-07-21 11:40:46 +0300

    handle unhandled routes

--- app.js
+++ app.js
@@ -21,4 +21,11 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/tours', tourRouter);
 app.use('/api/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
+
 module.exports = app;
