0e6a72a5d478ebe71ff9d381dd8e96d1a95a7865
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-07-21 22:40:36 +0300

    handle invalid Database IDs

--- middlewares/globalErrorMiddleware.js
+++ middlewares/globalErrorMiddleware.js
@@ -1,3 +1,7 @@
+const AppError = require('../util/AppError');
+
+const handleCastErrorDB = err => new AppError(`Invalid ${err.path}: ${err.value}`, 400);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -28,6 +32,9 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err, name: err.name };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
