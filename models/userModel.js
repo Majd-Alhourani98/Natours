55529b2ab049be42ed2474e44aadcfd8b5793a92
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-07-24 16:54:19 +0300

    add hash password functionality

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-
+const bcrypt = require('bcrypt');
 // User Schema
 const userSchema = new mongoose.Schema({
   name: {
@@ -36,9 +36,16 @@ const userSchema = new mongoose.Schema({
         return value === this.password;
       },
 
-      message: 'Passwords does not match',
+      message: 'Passwords are not the same',
     },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
 });
 
 const User = mongoose.model('User', userSchema);
