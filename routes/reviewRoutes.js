5c359b4d3e7884556e301ae6ebc297510628abd4
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-08-05 21:43:57 +0300

    populating Reviews

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -9,7 +9,9 @@ const {
   deleteReview,
 } = require('./../controllers/reviewController');
 
-router.route('/').get(getAllReviews).post(createReview);
+const { protect, restrictTo } = require('./../controllers/authController');
+
+router.route('/').get(getAllReviews).post(protect, restrictTo('user'), createReview);
 router.route('/:id').get(getSingleReview).patch(updateReview).delete(deleteReview);
 
 module.exports = router;
